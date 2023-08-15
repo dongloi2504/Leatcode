0fecd8b62ff53beae813126328dbb29902dd9e23
dongl <dongloi2504@gmail.com>
2023-08-15 23:44:55 +0700

    second commit

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,5 +1,6 @@
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        //hello cac ban
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
